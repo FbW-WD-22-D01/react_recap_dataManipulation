b2d492fe00b3edc785d49c41256b31306d3209ad
puckfried <puckfried@posteo.de>
2022-09-14 11:32:30 +0200

    added today

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Cart() {
   
@@ -18,8 +18,10 @@ function Cart() {
                 price: 0.5}
     ])
 
-    const [cart, setCart] = useState(  [    {name: '', productId:0, price: 0, amount: 0}   ]  )
-  
+    const [cart, setCart] = useState(  [      ]  )
+    const [total, setTotal] = useState(0)
+
+    
 function changePrice(e, id){
 
     const myArr = products.map ( el => {            //map wenn der Array gleiche Länge aber verändert, filter wenn gleicher Inhalt aber kürzer
@@ -73,7 +75,65 @@ function addToCart(id){
         // Wenn es nicht im Warenkorb ist, legen wir es neu hinein, ergänzen aber den amount, damit wir ihn später ehöhen können
         setCart([...cart, {...myObj, amount: 1}])
     }
+
+
+
 }
+
+
+function removeItem(id){
+    console.log('Soll gelöscht werden: ', id, ' aus dem Warenkorb: ', cart)
+    // Das Objekt suchen
+    const myObj = cart.find(el => el.id === id)
+    
+    // Den amount im Objekt reduzieren
+    myObj.amount = myObj.amount - 1 
+    
+    // überprüfen ob das Objekt noch da ist
+    if (myObj.amount > 0 ){
+        
+        // Wenn ja das Objekt im Warenkob verändern
+        const result = cart.map( el => {
+            if (el.id === id){
+                return myObj
+            } else {
+                return el
+            }
+        })
+        setCart(result)
+
+
+    } else {
+        // das Objekt aus Warenkorb löschen
+        console.log('Objekt soll raus aus Warenkorb')    
+        const result = cart.filter( el => {
+            if( el.id !== id){
+                return el
+            }
+        })  
+        setCart(result)      
+        
+    }
+}
+
+
+useEffect(() => {
+    //  const myTotalResult = cart.reduce( (acc, curr, index) => {
+    //             console.log('Acc: ', acc, 'Curent Value: ', curr)        
+    //             acc += curr.price * curr.amount
+    //             return acc
+    //         },0 )
+    
+    let result = 0
+    cart.map( (el, index, arr) => {
+        result += el.price*el.amount
+        
+    })
+    console.log(result)
+    // setTotal(myTotalResult)
+}, [cart])
+
+
 
   return (
     <div>
@@ -92,9 +152,24 @@ function addToCart(id){
             } )}
         </ul>
 
-        <p>Mein Warenkorb enthält: {cart.map( (el, index) => `${el.name} (${el.amount}) - `)}</p>
+        <p>Mein Warenkorb enthält: {cart.map( (el, index) => {
+           return (
+               <span key={index}> 
+                <span>{el.name} {el.amount}</span>
+                 <button onClick={() => removeItem(el.id)}>-</button> 
+                 <span> // </span>
+                </span>  )}
+           )}
+            
+            </p>
         
-        <p>Gesamtsumme:</p>
+        
+        <p>Gesamtsumme: {total}
+            {/* {cart.reduce( (acc, curr) => {
+                    acc += curr.price * curr.amount
+                return acc
+            },0 )  } */}
+         €</p>
     </div>
   )
 }
